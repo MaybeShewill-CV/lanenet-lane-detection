24a6a1b00f0d9ebdcb3cf124272a1cfabf80c17d
luoyao <luoyao@baidu.com>
2018-10-24 14:00:06 +0800

    modified lanenet post process functions

--- lanenet_model/lanenet_postprocess.py
+++ lanenet_model/lanenet_postprocess.py
@@ -28,7 +28,7 @@ class LaneNetPoseProcessor(object):
         pass
 
     @staticmethod
-    def morphological_process(image, kernel_size=5):
+    def _morphological_process(image, kernel_size=5):
         """
 
         :param image:
@@ -40,15 +40,15 @@ class LaneNetPoseProcessor(object):
         if len(image.shape) == 3:
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        kernel = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(kernel_size, kernel_size))
+        kernel = cv2.getStructuringElement(shape=cv2.MORPH_ELLIPSE, ksize=(kernel_size, kernel_size))
 
         # close operation fille hole
-        closing = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+        closing = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=1)
 
         return closing
 
     @staticmethod
-    def connect_components_analysis(image):
+    def _connect_components_analysis(image):
         """
 
         :param image:
@@ -61,7 +61,7 @@ class LaneNetPoseProcessor(object):
 
         return cv2.connectedComponentsWithStats(gray_image, connectivity=8, ltype=cv2.CV_32S)
 
-    def postprocess(self, image, minarea_threshold=200):
+    def postprocess(self, image, minarea_threshold=15):
         """
 
         :param image:
@@ -69,10 +69,10 @@ class LaneNetPoseProcessor(object):
         :return:
         """
         # 首先进行图像形态学运算
-        morphological_ret = self.morphological_process(image, kernel_size=5)
+        morphological_ret = self._morphological_process(image, kernel_size=5)
 
         # 进行连通域分析
-        connect_components_analysis_ret = self.connect_components_analysis(image=morphological_ret)
+        connect_components_analysis_ret = self._connect_components_analysis(image=morphological_ret)
 
         # 排序连通域并删除过小的连通域
         labels = connect_components_analysis_ret[1]
@@ -89,7 +89,7 @@ class LaneNetPoseProcessor(object):
 if __name__ == '__main__':
     processor = LaneNetPoseProcessor()
 
-    image = cv2.imread('binary_ret.png', cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread('test.png', cv2.IMREAD_GRAYSCALE)
 
     postprocess_ret = processor.postprocess(image)
 
