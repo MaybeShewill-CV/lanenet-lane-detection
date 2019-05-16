34e7231f41fb446888f556937512e4c1defb5493
MaybeShewill-CV <luoyao@baidu.com>
2019-05-16 20:55:25 +0800

    Update evaluate_lanenet_on_tusimple.py

--- tools/evaluate_lanenet_on_tusimple.py
+++ tools/evaluate_lanenet_on_tusimple.py
@@ -101,7 +101,7 @@ def test_lanenet_batch(src_dir, weights_path, save_dir):
 
             input_image_dir = ops.split(image_path.split('clips')[1])[0][1:]
             input_image_name = ops.split(image_path)[1]
-            output_image_dir = ops.join(dst_dir, input_image_dir)
+            output_image_dir = ops.join(save_dir, input_image_dir)
             os.makedirs(output_image_dir, exist_ok=True)
             output_image_path = ops.join(output_image_dir, input_image_name)
             if ops.exists(output_image_path):
