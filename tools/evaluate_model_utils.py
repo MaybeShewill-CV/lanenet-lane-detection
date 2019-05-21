ee3733cd2a9387694ee73dfb604bdfe7ce030e05
luoyao <luoyao@baidu.com>
2019-05-21 15:55:55 +0800

    change evaluate figure compute method

--- tools/evaluate_model_utils.py
+++ tools/evaluate_model_utils.py
@@ -43,8 +43,10 @@ def calculate_model_fp(input_tensor, label_tensor):
     final_output = tf.expand_dims(tf.argmax(logits, axis=-1), axis=-1)
 
     idx = tf.where(tf.equal(final_output, 1))
-    pix_cls_ret = tf.gather_nd(label_tensor, idx)
-    false_pred = tf.cast(tf.shape(pix_cls_ret)[0], tf.int64) - tf.count_nonzero(pix_cls_ret)
+    pix_cls_ret = tf.gather_nd(final_output, idx)
+    false_pred = tf.cast(tf.shape(pix_cls_ret)[0], tf.int64) - tf.count_nonzero(
+        tf.gather_nd(label_tensor, idx)
+    )
 
     return tf.divide(false_pred, tf.cast(tf.shape(pix_cls_ret)[0], tf.int64))
 
@@ -61,9 +63,10 @@ def calculate_model_fn(input_tensor, label_tensor):
 
     idx = tf.where(tf.equal(label_tensor, 1))
     pix_cls_ret = tf.gather_nd(final_output, idx)
-    mis_pred = tf.cast(tf.shape(pix_cls_ret)[0], tf.int64) - tf.count_nonzero(pix_cls_ret)
+    label_cls_ret = tf.gather_nd(label_tensor, tf.where(tf.equal(label_tensor, 1)))
+    mis_pred = tf.cast(tf.shape(label_cls_ret)[0], tf.int64) - tf.count_nonzero(pix_cls_ret)
 
-    return tf.divide(mis_pred, tf.cast(tf.shape(pix_cls_ret)[0], tf.int64))
+    return tf.divide(mis_pred, tf.cast(tf.shape(label_cls_ret)[0], tf.int64))
 
 
 def get_image_summary(img):
