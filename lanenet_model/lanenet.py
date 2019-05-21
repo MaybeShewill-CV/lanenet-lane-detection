b9a6c2f2829acb4f69d61803dd783ee650f3fd5e
luoyao <luoyao@baidu.com>
2019-05-21 15:58:56 +0800

    remove useless import

--- lanenet_model/lanenet.py
+++ lanenet_model/lanenet.py
@@ -11,7 +11,6 @@ Implement LaneNet Model
 import tensorflow as tf
 
 from config import global_config
-from data_provider import lanenet_data_feed_pipline
 from lanenet_model import lanenet_back_end
 from lanenet_model import lanenet_front_end
 from semantic_segmentation_zoo import cnn_basenet
