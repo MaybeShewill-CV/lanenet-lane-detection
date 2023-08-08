5b0a37f2c29f03ec4394c327364588884a498fda
JayGupta2003 <jaygupta010203@gmail.com>
2023-08-08 12:57:51 -0400

    Upgrade Files to Use TF-2 using tf2_upgrade script

---
+++ upgraded/lanenet_model_upgraded/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 18-5-11 上午11:32
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/lanenet-lane-detection
+# @File    : __init__.py.py
+# @IDE: PyCharm Community Edition
---
+++ upgraded/local_utils_upgraded/config_utils/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/6/11 下午5:46
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/lanenet-lane-detection
+# @File    : __init__.py
+# @IDE: PyCharm
